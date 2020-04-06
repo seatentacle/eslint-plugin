ff8f7dc8993ad8e7ff95606f6877dd8a4a08a834
Dmitry Gusarov <sea.tentacle@gmail.com>
2020-04-06 15:12:39 +0300

    fix: js config and no-extra-pares rule

--- src/configs/javascript.js
+++ src/configs/javascript.js
@@ -7,39 +7,39 @@ module.exports = {
       impliedStrict: true,
       jsx: true,
     },
-    env: {
-      browser: true,
-      node: true,
-      jest: true,
-    },
-    globals: {
-      shallow: 'readonly',
-      mount: 'readonly',
-      render: 'readonly',
-    },
-    plugins: [
-      '@seatentacle',
-      'import',
-      'promise',
-      'react',
-      'react-hooks',
-    ],
-    extends: [
-      'eslint:recommended',
-    ],
-    rules: {
-      'no-await-in-loop': 'error',
-      'no-console': ['error', {
-        allow: ['error'],
-      }],
-      'no-extra-parens': ['error', 'all', {
-        returnAssign: false,
-        nestedBinaryExpressions: false,
-        ignoreJsx: 'multiline',
-      }],
-      'no-template-curly-in-string': 'error',
-      'no-useless-backreference': 'error',
-      'require-atomic-updates': 'error',
-    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  globals: {
+    shallow: 'readonly',
+    mount: 'readonly',
+    render: 'readonly',
+  },
+  plugins: [
+    '@seatentacle',
+    'import',
+    'promise',
+    'react',
+    'react-hooks',
+  ],
+  extends: [
+    'eslint:recommended',
+  ],
+  rules: {
+    'no-await-in-loop': 'error',
+    'no-console': ['error', {
+      allow: ['error'],
+    }],
+    'no-extra-parens': ['error', 'all', {
+      returnAssign: false,
+      nestedBinaryExpressions: false,
+      ignoreJSX: 'multi-line',
+    }],
+    'no-template-curly-in-string': 'error',
+    'no-useless-backreference': 'error',
+    'require-atomic-updates': 'error',
   },
 };
